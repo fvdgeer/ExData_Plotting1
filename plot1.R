e42bc34412566515017ee7a6d22d3ddae202ae89
Frans van der Geer <fvdgeer@vxcompany.com>
2015-10-08 17:28:47 +0200

    plot1 and 2 done

---
+++ plot1.R
@@ -0,0 +1,34 @@
+# Course Project1 -- Exploratory Data Analysis
+# By fvdgeer
+library(readr)
+library(dplyr)
+library(lubridate)
+
+dataUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+targetDir <- file.path(".", "data")
+targetFile <- file.path(targetDir, "household_power_consumption.zip")
+dataFile <- sub("zip", "txt", targetFile)
+
+if (!dir.exists(targetDir)) dir.create(targetDir)
+if (!file.exists(targetFile)) download.file(dataUrl, targetFile)
+if (!file.exists(dataFile)) unzip(targetFile, exdir=targetDir)
+
+powerdata<-read_csv2(dataFile, na="?", col_names=TRUE, col_types = list(
+            Date=col_date(format = "%d/%m/%Y"), 
+            Time=col_character(),
+            Global_active_power=col_double(),
+            Global_reactive_power=col_double(),
+            Voltage=col_double(),
+            Global_intensity=col_double(),
+            Sub_metering_1=col_double(),
+            Sub_metering_2=col_double(),
+            Sub_metering_3=col_double() ))
+    
+powerdata %>% 
+    filter(year(Date)==2007 & month(Date)==2 & day(Date) %in% c(1,2)) %>%
+    mutate(Timestamp=as.POSIXct(paste(Date, Time))) %>%
+    select(Timestamp, Global_active_power) -> plot2data
+
+with(plot2data, plot(Timestamp, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
+dev.off(dev.copy(png, "plot2.png"))
+
